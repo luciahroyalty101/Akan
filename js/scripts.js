005dda2211ec6173609bbbb593417ba3a75cc5d4
luciahroyalty101 <luciahkei101@gmail.com>
2020-10-02 17:48:06 +0300

    modify js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
-var dayOfTheWeek=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var maleAkanName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleAkanName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var dayOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var maleAkanName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-function akan(){
-    var year=document.parseInt(getElementById("year").value);
-    var month=document.parseInt(getElementById("month").value);
-    var date=document.parseInt(getElementById("date").value);
+function akan() {
+    var year = document.getElementById("year").value;
+    var month = parseInt(document.getElementById("month").value);
+    var date = parseInt(document.getElementById("date").value);
+    var gender = document.getElementById("gender").value;
+
+    var CC = parseInt(year.substr(0, 2));
+    prompt(CC);
+    var YY = parseInt(year.substr(2, 3));
+    // prompt(cc)
+
+    
+    prompt(YY)
+
+
 }
-var YY="Year"
-var MM="Month"
-var DD="Date"
-
-
-D(d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
-
- 
