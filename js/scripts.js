bf67a6901a2e8a195fa9e5cacc9605ea1ca398f9
luciahroyalty101 <luciahkei101@gmail.com>
2020-10-02 18:42:42 +0300

    add method

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ function akan() {
     var MM = parseInt(month.substr(5,6));
     var DD = parseInt(date.substr(8,9));
     // prompt(cc)
-
+    var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
     
     prompt(YY)
 
