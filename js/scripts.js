d7d044c14a3a3048cba251d499f5af129505207b
luciahroyalty101 <luciahkei101@gmail.com>
2020-10-02 16:01:11 +0300

    add variables

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var dayOfTheWeek=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var maleAkanName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+var femaleAkanName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+function akan(){
+    var year=document.parseInt(getElementById("year").value);
+    var month=document.parseInt(getElementById("month").value);
+    var date=document.parseInt(getElementById("date").value);
+}
