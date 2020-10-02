9d01af83bd40461136e237d7c00c87a8f4760821
luciahroyalty101 <luciahkei101@gmail.com>
2020-10-02 23:58:47 +0300

    edit functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,36 @@ var femaleAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 function akan() {
     var year = document.getElementById("year").value;
     var month = parseInt(document.getElementById("month").value);
-    var date = parseInt(document.getElementById("date").value);
+    var Date = parseInt(document.getElementById("date").value);
     var gender = document.getElementById("gender").value;
-
     var CC = parseInt(year.substr(0, 2));
-    prompt(CC);
-    var YY = parseInt(year.substr(2, 3));
-    var MM = parseInt(month.substr(5,6));
-    var DD = parseInt(date.substr(8,9));
-    // prompt(cc)
-    var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-    
-    prompt(YY)
+    var YY = parseInt(year.substr(2, 4)); 
+    var MM = month;
+    var DD = Date;
+    var day = parseInt((5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7
+
+    if (gender === "Female") {
+        alert("Akan name is " + femaleAkanName[day])
+    } else {
+        alert("Akan name is " + maleAkanName[day])
+        
+    }
+    if (DD=>0||DD<32) {
+        // alert("Valid date")
+    } else {
+        alert("Enter a valid date")
+    }
+    if (MM=>o||MM<12) {
+        // alert("valid month")
+    } else {
+         alert("Enter a valid month")
+            }
+            if (YY=>1800||YY<2020) {
+                // alert("valid year")
+            } else {
+                alert("Enter a valid year")
+            }
+           
 
 
 }
